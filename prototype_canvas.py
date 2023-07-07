7119be21991fbb4c46f4744c31a19562f9e07204
Eric Zou <ezou626@gmail.com>
2023-07-06 21:46:48 -0400

    Prototyping app to create a better drawing brush

---
+++ prototype_canvas.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+import numpy as np
+
+'''
+Prototyping app to create a better drawing brush for the real thing
+'''
+
+class PaintApp():
+
+if __name__ == '__main__':
+    
+    root = tk.Tk()
